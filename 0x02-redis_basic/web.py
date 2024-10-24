18f100b9efd68b7af5201b1312c942f73021358c
tevn23 <mabulatt23@gmail.com>
2024-10-24 09:33:41 +0200

    Documentation fix

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -7,17 +7,26 @@ from datetime import timedelta
 
 
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response    and tracking the request.
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
     '''
     if url is None or len(url.strip()) == 0:
         return ''
+    # Initialize Redis client
     redis_store = redis.Redis()
+    # Define keys for caching and request count
     res_key = 'result:{}'.format(url)
     req_key = 'count:{}'.format(url)
+    # Try to get the cached result
     result = redis_store.get(res_key)
+    # Increment the request count whether cache hit or miss
+    redis_store.incr(req_key)
     if result is not None:
-        redis_store.incr(req_key)
-        return result
+        # Decode the result from bytes to string if it's cached
+        return result.decode('utf-8')
+    # Fetch the result from the URL if not cached
     result = requests.get(url).content.decode('utf-8')
+    # Cache the result with an expiration of 10 seconds
     redis_store.setex(res_key, timedelta(seconds=10), result)
+    
     return result
